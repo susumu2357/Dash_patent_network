5ccea84b9f8ebfee7e6febf1f5333f5e3f5b5b45
susumu2357 <susumu.0112357@gmail.com>
2020-04-11 15:10:30 +0900

    change debug mode to False

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ import plotly.graph_objects as go
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 def dump(fname, obj):
     with gzip.open(fname, 'wb') as f:
@@ -258,4 +259,4 @@ def display_click_data(selectedData):
         return [html.Tr([html.Td("") for _ in range(4)])]
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
